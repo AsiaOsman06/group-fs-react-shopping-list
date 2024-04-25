7f95ff392f28a40bfa5afbb6db43cb5453c957f4
Mustafe Hassan <mustafeh123@gmail.com>
2024-04-25 16:22:06 -0500

    Inital commit #2

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5001;
+const shoppingListRouter = require('./routes/shoppingList.router.js')
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for axios requests
@@ -10,7 +11,7 @@ app.use(express.static('build'));
 /** ---------- EXPRESS ROUTES ---------- **/
 // Create your API routes in a separate file
 // and plug them in here with `app.use()`
-app.use('/api/shoppinglist', guestRouter);
+app.use('/api/shoppinglist', shoppingListRouter);
 
 
 /** ---------- START SERVER ---------- **/
