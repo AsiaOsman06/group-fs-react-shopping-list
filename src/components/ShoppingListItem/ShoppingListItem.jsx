524b28ea0332a3f8f2d7cf2a03baa8923b23b0e8
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-04-25 16:43:42 -0500

    correct parameter

--- src/components/ShoppingListItem/ShoppingListItem.jsx
+++ src/components/ShoppingListItem/ShoppingListItem.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 
-function ShoppingListItem({ key, item, fetchShoppingList }){
+function ShoppingListItem({ item, fetchShoppingList }){
 
     const buyItem = () => {
         axios({
