4442b0a6f76bfd7eaddf982e256fa0305602acbe
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-04-25 16:42:13 -0500

    add parameter to shoppingListItem component

--- src/components/ShoppingListItem/ShoppingListItem.jsx
+++ src/components/ShoppingListItem/ShoppingListItem.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 
-function ShoppingListItem({ item, fetchShoppingList }){
+function ShoppingListItem({ key, item, fetchShoppingList }){
 
     const buyItem = () => {
         axios({
