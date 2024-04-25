c506ed984d0f9029c81654c274f9325439cfa343
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-04-25 15:32:04 -0500

    troubleshooting button functions

--- src/components/ShoppingListItem/ShoppingListItem.jsx
+++ src/components/ShoppingListItem/ShoppingListItem.jsx
@@ -1,8 +1,9 @@
-import react from "react";
+import React from "react";
 import axios from "axios";
 
-function ShoppingListItem(shoppingList){
-    const buyItem = ({item.id}) => {
+function ShoppingListItem({item}){
+
+    const buyItem = (item.id) => {
         axios({
             method: "PUT",
             url: `/api/shoppingList/${item.id}`,
@@ -12,5 +13,34 @@ function ShoppingListItem(shoppingList){
             //Actual function name TBD
             fetchShoppingList();
         })
-    }
+        .catch ((error)=>{
+            console.log ("Unable to buy item: ", error)
+        })
+    };
+
+    const removeItem({item.id})=>{
+        axios({
+            method: 'DELETE',
+            url: `/api/shoppingList/${item.id}`,
+        })
+        .then(response => {
+            //Actual function name TBD
+            fetchShoppingList();
+        })
+        .catch ((error)=>{
+            console.log ("Unable to remove item: ", error)
+        })
+    };
+
+    return (
+    <article key={item.id}>
+                    {item.name}
+                    {item.quantity} {item.unit}
+                    {!item.isPurchased ? <button onClick={() => buyItem(item.id)}>Buy</button> : null}
+                    <button onClick={() => removeItem(item.id)}>Remove</button>
+            ;
+    </article>
+);
 }
+
+export default ShoppingListItem;
